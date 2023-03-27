59f40a42d388f6c3c6d79a6785baf00ab8d139e9
sundaymark <sundayukpai1122@gmail.com>
2023-03-27 18:38:54 +0100

    created three variables to house the first number the math operator second number

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const add = function(a,b){
-    return a + b;
-}
+ const add = (a, b) =>  a  +  b;
 
-const subtract = function(a,b){
-    return a - b;
-}
+const subtract = (a, b) => a  -  b;
 
-const multiply = function(a,b){
-    return a * b;
-}
+const multiply = (a, b) =>  a  *  b;
 
-const divide = function(a,b){
-    return a / b;
-}
+const divide = (a, b) =>  a  /  b;
+
+const firstNumber = '';
+const operator = '';
+const secondNumber = '';
+
 
 
 
