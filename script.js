635d197e76b88d02fc76022bd69f28db23979d13
sundaymark <sundayukpai1122@gmail.com>
2023-03-27 19:42:11 +0100

    created the operate function that takes an operator and two numbers

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const firstNumber = '';
 const operator = '';
 const secondNumber = '';
 
+const operate = (operator, firstNumber,secondNumber)=>{
+    if(operator === '+'){
+        return add(firstNumber,secondNumber);
+    }else if(operator === '-'){
+        return subtract(firstNumber,secondNumber)
+    }else if ( operator === '*'){
+        return multiply(firstNumber,secondNumber)
+    }else if (operator === '/'){
+        return divide(firstNumber,secondNumber);
+    }
+}
+
+console.log(operate('+',5,5))
+
+
+
+
 
 
 
